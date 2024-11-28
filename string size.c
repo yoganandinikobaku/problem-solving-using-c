d0106773b2d7c67d0728a064b4aaa5be2b525537
yoganandinikobaku <yoganandinikobaku5@gmail.com>
2024-11-28 16:51:59 +0530

    Create string size.c

---
+++ string size.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  char name[5] = {'H','a','i'};
+  printf("\n size of name = %d",size of(name));
+}
+{
+  char name[] = {'H','e','l','l','o'};
+  int i,size;
+  size = size of (name);
+  printf("\n size of name = %d\n",size);
+  for(i=0;i<size;i++) 
+  printf("%c",name[i]);
+}
+{
+  char name[5];
+  int i,size;
+  size = size of (name);
+  printf("\n size of name = %d\n",size);
+  printf("\n enter string\n");
+  gets(name);
+  printf("\n given string\n");
+  puts(name);
+} 
