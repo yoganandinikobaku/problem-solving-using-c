2a29983a44e3bbce145f815a58ea42fb8dc5792c
yoganandinikobaku <yoganandinikobaku5@gmail.com>
2024-11-28 16:19:46 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  char str[100];
+  printf("enter a string: ");
+  scanf("%s",str); //reading a string from user
+  printf("you entered: %s\n",str); //printing the string
+  return 0;
+}
