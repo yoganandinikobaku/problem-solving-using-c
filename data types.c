20c1f0e8e826c788788375fbe422718b81c992af
yoganandinikobaku <yoganandinikobaku5@gmail.com>
2024-11-28 16:11:16 +0530

    Create data types.c

---
+++ data types.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+  printf("size of int: %1u bytes\n",size of (int));
+  printf("size of char: %1u bytes\n",size of (char));
+  printf("size of float: %1u bytes\n",size of (float));
+  return 0;
+} 
