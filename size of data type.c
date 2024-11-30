d3446abc0dc90b387a837086f33b580dfc70088c
yoganandinikobaku <yoganandinikobaku5@gmail.com>
2024-11-30 12:38:20 +0530

    Create size of data type.c

---
+++ size of data type.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int size_of_int = size of(int);
+  int size_of_char = size of(char);
+  int size_of_float = size of(float);
+  int size_of_double = size of(double);
+  clrscr();
+  printf("the size of int data type: %d\n",size_of_int);
+  printf("the size of char data type: %d\n",size_of_char);
+  printf("the size of float data type: %d\n",size_of_float);
+  printf("the size of double data type: %d\\n",size_of_double);
+  getch();
+  return 0;
+} 
