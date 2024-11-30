53f4e3effbbff4cf01781a3a7505d75126ecd557
yoganandinikobaku <yoganandinikobaku5@gmail.com>
2024-11-30 12:52:17 +0530

    Create types of integers.c

---
+++ types of integers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{ 
+  clrscr();
+  printf("maximum integer value: %d\n",int_max);
+  printf("minimum integer value: %d\n",int_min);
+  print("maximum long value: %1d\n",long_max);
+  printf("minimum long value: %1d\n",long_min);
+  getch();
+  return 0;
+} 
