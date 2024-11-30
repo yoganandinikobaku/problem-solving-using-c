ad0dabcf6dc0d358bebc401a04be09cf1ad58011
yoganandinikobaku <yoganandinikobaku5@gmail.com>
2024-11-30 11:58:33 +0530

    Create heart.c

---
+++ heart.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+ int main()
+ { 
+    // Print the heart shape using asterisks 
+    printf("  **     **  \n"); 
+    printf(" *  *   *  * \n"); 
+    printf("*    * *    *\n"); 
+    printf("*            *\n"); 
+    printf(" *           * \n"); 
+    printf("  *         *  \n"); 
+    printf("   *       *   \n"); 
+    printf("    *     *    \n"); 
+    printf("     *   *     \n"); 
+    printf("      * *      \n"); 
+    printf("       *       \n"); 
+    return 0; 
+}  
