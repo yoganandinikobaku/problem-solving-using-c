98e8366a418c076f69ccc87ddfe5bfc3b9bd3dad
yoganandinikobaku <yoganandinikobaku5@gmail.com>
2024-12-01 12:40:54 +0530

    Create unformatted  I\O.c

---
+++ unformatted  I\O.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   char ch;
+   clrscr();
+   printf("press any character: ");
+   ch = getchar();
+   printf("\n you pressed: ");
+   putchar(ch);
+   getch();
+} 
