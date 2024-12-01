d22241c674a9d45cb35a55bd7d79e9c57ace9006
yoganandinikobaku <yoganandinikobaku5@gmail.com>
2024-12-01 11:25:38 +0530

    Create while loop.c

---
+++ while loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  //local variable definition
+  int a = 1;
+  //while loop execution
+  do
+  {
+     printf("hello world\n");
+     a++;
+  }
+  while(a<=5);
+  printf("end of loop");
+  return 0;
+} 
