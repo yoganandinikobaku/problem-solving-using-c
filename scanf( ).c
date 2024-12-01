aa6dc1022fda77941a81ec5964454392cd04f855
yoganandinikobaku <yoganandinikobaku5@gmail.com>
2024-12-01 10:45:33 +0530

    Create scanf( ).c

---
+++ scanf( ).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int num1;
+   //printing a message on
+   //the output screen
+   printf("enter a integer number: ");
+   //taking an integer value
+   //from keyboard
+   scanf("%d",&num1);
+   //displaying the entered value
+   printf("you have entered %d",num1);
+   return 0;
+}
